e42bffb3ccfbdb048716fdb58c90d8a3383b10f9
galyan90 <gal.yaniv90@gmail.com>
2025-06-05 10:49:26 +0300

    Update GameScreen.js

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,1 +1,44 @@
-// TODO: Implement GameScreen
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { GameContext } from '../state/GameContext';
+import WordCard from '../components/WordCard';
+
+export default function GameScreen() {
+  const { board, currentTurn } = useContext(GameContext);
+
+  const renderItem = ({ item }) => (
+    <WordCard word={item.word} revealed={item.revealed} />
+  );
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.turnText}>
+        Current Turn: {currentTurn === 'red' ? '🔴 Red' : '🔵 Blue'}
+      </Text>
+
+      <FlatList
+        data={board}
+        numColumns={5}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+        contentContainerStyle={styles.board}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: '#f6f6f6'
+  },
+  turnText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10
+  },
+  board: {
+    justifyContent: 'center'
+  }
+});
