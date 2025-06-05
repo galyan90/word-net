00634c99af3f4283052b93f0250a8c680f9aee29
galyan90 <gal.yaniv90@gmail.com>
2025-06-05 11:09:14 +0300

    Update reducer.js

--- state/reducer.js
+++ state/reducer.js
@@ -1,10 +1,28 @@
-export default function reducer(state, action) {
-  switch (action.type) {
-    case 'SET_GAME_ID':
-      return { ...state, gameId: action.payload };
-    case 'SET_PLAYERS':
-      return { ...state, players: action.payload };
+// state/reducer.js - לוגיקת עדכון המצב
+export const initialState = {
+  screen: 'home',
+  board: [],
+  currentTeam: 'red',
+  phase: 'spymaster',
+  score: { red: 9, blue: 8 },
+  clue: { word: '', number: 0 },
+  guessesLeft: 0,
+  revealed: new Set(),
+  winner: null
+};
+
+const gameReducer = (state, action) => {
+  switch(action.type) {
+    case 'SET_SCREEN':
+      return { ...state, screen: action.payload };
+    case 'INIT_GAME':
+      return { ...state, ...action.payload };
+    case 'SELECT_CARD':
+      return { ...state, revealed: new Set([...state.revealed, action.payload]) };
+    // ... עוד actions
     default:
       return state;
   }
-}
+};
+
+export default gameReducer;
