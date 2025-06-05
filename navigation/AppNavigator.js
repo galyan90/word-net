c80b23114c2c6c605242481476baec9c7334dfe4
galyan90 <gal.yaniv90@gmail.com>
2025-06-05 11:25:05 +0300

    Update AppNavigator.js

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,23 +1,42 @@
-// navigation/AppNavigator.js - ניווט בין מסכים
-import React, { useContext } from 'react';
-import { GameContext } from '../state/GameContext';
+// navigation/AppNavigator.js - ניווט בין מסכי המשחק
+
+import React from 'react';
+import { useGame } from '../state/GameContext';
 import HomeScreen from '../screens/HomeScreen';
+import LobbyScreen from '../screens/LobbyScreen';
 import GameScreen from '../screens/GameScreen';
 import EndGameScreen from '../screens/EndGameScreen';
 
 const AppNavigator = () => {
-  const { gameState } = useContext(GameContext);
-  
-  switch(gameState.screen) {
-    case 'home':
-      return <HomeScreen />;
-    case 'game':
-      return <GameScreen />;
-    case 'end':
-      return <EndGameScreen />;
-    default:
-      return <HomeScreen />;
-  }
+  const { gameState } = useGame();
+
+  // רינדור המסך המתאים לפי המצב הנוכחי
+  const renderScreen = () => {
+    switch (gameState.screen) {
+      case 'home':
+        return <HomeScreen />;
+        
+      case 'lobby':
+        return <LobbyScreen />;
+        
+      case 'game':
+        return <GameScreen />;
+        
+      case 'end':
+        return <EndGameScreen />;
+        
+      default:
+        // אם יש מצב לא מוכר, חזור לבית
+        console.warn(`Unknown screen: ${gameState.screen}`);
+        return <HomeScreen />;
+    }
+  };
+
+  return (
+    <div className="app-navigator">
+      {renderScreen()}
+    </div>
+  );
 };
 
 export default AppNavigator;
