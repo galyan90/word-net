5e07a536448a906f551a8dd96cbf0c8149295673
galyan90 <gal.yaniv90@gmail.com>
2025-06-05 11:12:26 +0300

    Update colors.js

--- constants/colors.js
+++ constants/colors.js
@@ -1,1 +1,42 @@
-export default { primary: '#6200ee', background: '#f6f6f6' };
+// constants/colors.js - צבעים וסגנונות המשחק
+
+export const CARD_COLORS = {
+  hidden: 'card-hidden',
+  red: 'card-red',
+  blue: 'card-blue',
+  neutral: 'card-neutral',
+  assassin: 'card-assassin'
+};
+
+export const TEAM_COLORS = {
+  red: '#e74c3c',
+  blue: '#3498db'
+};
+
+export const CARD_STYLES = {
+  hidden: {
+    backgroundColor: '#f7fafc',
+    borderColor: '#cbd5e0',
+    color: '#4a5568'
+  },
+  red: {
+    backgroundColor: '#e74c3c',
+    borderColor: '#c0392b',
+    color: 'white'
+  },
+  blue: {
+    backgroundColor: '#3498db',
+    borderColor: '#2980b9',
+    color: 'white'
+  },
+  neutral: {
+    backgroundColor: '#95a5a6',
+    borderColor: '#7f8c8d',
+    color: 'white'
+  },
+  assassin: {
+    backgroundColor: '#2c3e50',
+    borderColor: '#1a252f',
+    color: 'white'
+  }
+};
