7091f48ccecd0269f06c35ec1cdd9f5d4eecdcb1
galyan90 <gal.yaniv90@gmail.com>
2025-06-05 11:10:56 +0300

    Update GameBoard.js

--- components/GameBoard.js
+++ components/GameBoard.js
@@ -1,1 +1,18 @@
-// TODO: Implement GameBoard
+// components/GameBoard.js - לוח המשחק
+import React, { useContext } from 'react';
+import { GameContext } from '../state/GameContext';
+import WordCard from './WordCard';
+
+const GameBoard = () => {
+  const { gameState } = useContext(GameContext);
+  
+  return (
+    <div className="game-board">
+      {gameState.board.map(card => (
+        <WordCard key={card.id} card={card} />
+      ))}
+    </div>
+  );
+};
+
+export default GameBoard;
