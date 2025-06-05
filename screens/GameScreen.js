30b37f698f93cbfb7cdb5b548ed44fd1739e1f5f
galyan90 <gal.yaniv90@gmail.com>
2025-06-05 11:10:35 +0300

    Update GameScreen.js

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,44 +1,20 @@
+// screens/GameScreen.js - מסך המשחק
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
 import { GameContext } from '../state/GameContext';
-import WordCard from '../components/WordCard';
+import GameBoard from '../components/GameBoard';
 
-export default function GameScreen() {
-  const { board, currentTurn } = useContext(GameContext);
-
-  const renderItem = ({ item }) => (
-    <WordCard word={item.word} revealed={item.revealed} />
-  );
-
+const GameScreen = () => {
+  const { gameState, dispatch } = useContext(GameContext);
+  
   return (
-    <View style={styles.container}>
-      <Text style={styles.turnText}>
-        Current Turn: {currentTurn === 'red' ? '🔴 Red' : '🔵 Blue'}
-      </Text>
-
-      <FlatList
-        data={board}
-        numColumns={5}
-        renderItem={renderItem}
-        keyExtractor={(item, index) => index.toString()}
-        contentContainerStyle={styles.board}
-      />
-    </View>
+    <div className="game-screen">
+      <div className="game-header">
+        <span>תור: {gameState.currentTeam}</span>
+        <span>ניקוד: {gameState.score.red}:{gameState.score.blue}</span>
+      </div>
+      <GameBoard />
+    </div>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: '#f6f6f6'
-  },
-  turnText: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 10
-  },
-  board: {
-    justifyContent: 'center'
-  }
-});
+export default GameScreen;
