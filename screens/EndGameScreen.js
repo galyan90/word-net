c2ee7ea6c3e83054212be109c779b47c55b923dd
galyan90 <gal.yaniv90@gmail.com>
2025-06-05 11:10:21 +0300

    Update EndGameScreen.js

--- screens/EndGameScreen.js
+++ screens/EndGameScreen.js
@@ -1,1 +1,20 @@
-// TODO: Implement EndGameScreen
+// screens/EndGameScreen.js - מסך סיום
+import React, { useContext } from 'react';
+import { GameContext } from '../state/GameContext';
+
+const EndGameScreen = () => {
+  const { gameState, dispatch } = useContext(GameContext);
+  
+  const newGame = () => {
+    dispatch({ type: 'SET_SCREEN', payload: 'home' });
+  };
+  
+  return (
+    <div className="end-screen">
+      <h2>{gameState.winner} ניצח!</h2>
+      <button onClick={newGame}>משחק חדש</button>
+    </div>
+  );
+};
+
+export default EndGameScreen;
